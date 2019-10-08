9e7e027d40f4b76d7fca752e1b45591954477877
Anastasiya Borodina <borodina-a.b@yandex.ru>
2019-10-08 13:58:51 +0300

    Add Person class

---
+++ TestTask/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTask
+{
+    public class Person
+    {
+        public Person Cheif { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Born { get; set; }
+    }
+}
